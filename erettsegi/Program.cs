998210dc1f917e8655f52b8dccb68f3a0c6af688
benceb1 <bence.b1.1.2@gmail.com>
2020-12-22 14:55:37 +0100

    1.feladat (beolvas)

--- erettsegi/Program.cs
+++ erettsegi/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace erettsegi
 {
@@ -11,23 +13,41 @@ namespace erettsegi
         public string szelerosseg { get; set; }
         public int homerseklet { get; set; }
 
+        public override string ToString()
+        {
+            return $"település:{telepules}, idő:{ido.ToString()}, szélirány:{szelirany}, szélerősség:{szelerosseg}, hőmérséklet:{homerseklet}";
+        }
     }
 
     class Program
     {
+        public static List<Adat> adatok = new List<Adat>();
         static void Main(string[] args)
         {
-            convertStringToTimestamp("1122");
+            beolvas();
+            foreach (var item in adatok)
+            {
+                Console.WriteLine(item);
+            }
         }
 
         public static void beolvas()
         {
-            StreamReader sr = new StreamReader("tavirathu13");
+            StreamReader sr = new StreamReader("tavirathu13.txt");
             while(!sr.EndOfStream)
             {
                 string[] sor = sr.ReadLine().Split(' ');
-
+                Adat adat = new Adat()
+                {
+                    telepules = sor[0],
+                    ido = convertStringToTimestamp(sor[1]),
+                    szelirany = sor[2].Substring(0, 3),
+                    szelerosseg = sor[2].Substring(3, 2),
+                    homerseklet = int.Parse(sor[3])
+                };
+                adatok.Add(adat);
             }
+            sr.Close();
         }
 
         public static TimeSpan convertStringToTimestamp(string szoveg) // 0000
