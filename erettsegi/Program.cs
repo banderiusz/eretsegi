9f305760e3f9f8b9335cdf224203144aa6254252
benceb1 <bence.b1.1.2@gmail.com>
2020-12-22 14:29:07 +0100

    kezdo-lepesek

---
+++ erettsegi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace erettsegi
+{
+    public class Adat
+    {
+        public string telepules { get; set; }
+        public TimeSpan ido { get; set; }
+        public string szelirany { get; set; }
+        public string szelerosseg { get; set; }
+        public int homerseklet { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            convertStringToTimestamp("1122");
+        }
+
+        public static void beolvas()
+        {
+            StreamReader sr = new StreamReader("tavirathu13");
+            while(!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(' ');
+
+            }
+        }
+
+        public static TimeSpan convertStringToTimestamp(string szoveg) // 0000
+        {
+            int ora = int.Parse(szoveg.Substring(0,2));
+            int perc = int.Parse(szoveg.Substring(2,2));
+            return new TimeSpan(ora, perc, 00);
+        }
+    }
+}
